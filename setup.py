511c8a5871c4fcde1ddfc5110beda0e1eb260299
arsrus721 <100342138+arsrus721@users.noreply.github.com>
2025-11-05 22:44:43 +0400

    Bump version to 1.2.0 and update description
    Updated version and description for the package.

--- setup.py
+++ setup.py
@@ -3,16 +3,17 @@ from setuptools import setup, find_packages
 
 setup(
     name="maxclientapi",
-    version="1.0.0",
+    version="1.2.0",
     packages=find_packages(),
-    install_requires=["requests", "websocket-client"],  # зависимости, если есть
+    install_requires=["requests", "websocket-client"], 
     author="arsrus721",
-    description="Моя первая библиотека Python",
+    description="A Python library for working with the 'MAX Messenger' WebSocket API",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/arsrus721/maxclientapi",  # если есть репозиторий
+    url="https://github.com/arsrus721/maxclientapi", 
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
     ],
 )
+
