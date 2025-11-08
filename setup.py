f433c87a7d9d4eab81a0efcedbeeea273c6708f0
arsrus721 <tskback13@gmail.com>
2025-11-08 17:38:42 +0500

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="maxclientapi",
-    version="2.0.0",
+    version="2.1.0",
     packages=find_packages(),
     install_requires=["websocket-client"], 
     author="arsrus721",
